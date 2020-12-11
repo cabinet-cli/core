51ea11056f8a3f250f518bc6bcdafcacc6b9272a
Sophia <beyondthesophia@gmail.com>
2020-12-11 15:03:16 +0900

    fix(provider): make provider throw an error when given name is invalid

--- src/Provider.spec.ts
+++ src/Provider.spec.ts
@@ -10,5 +10,17 @@ describe("Provider", () => {
 
         const provider = new ExampleProvider();
         expect(provider.name).toEqual("example");
-    })
-})
+    });
+
+    it("throws an error when invalid name provided", () => {
+        class ExampleProvider extends Provider {
+            constructor() {
+                super("");
+            }
+        }
+
+        expect(() => {
+            new ExampleProvider();
+        }).toThrow("Given provider name '' is not valid");
+    });
+});
--- src/Provider.ts
+++ src/Provider.ts
@@ -1,3 +1,7 @@
 export default abstract class Provider {
-    protected constructor(public readonly name: string) {}
+    protected constructor(public readonly name: string) {
+        if (!this.name) {
+            throw new Error(`Given provider name '${this.name}' is not valid`);
+        }
+    }
 }
